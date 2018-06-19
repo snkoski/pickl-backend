55bc4b0c1da58952041d5a01720dcd2c32458bd7
snkoski <shawnnkoski@gmail.com>
2018-06-19 16:12:01 -0400

    likes model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_200610) do
+ActiveRecord::Schema.define(version: 2018_06_19_201111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2018_06_19_200610) do
     t.integer "user_id"
     t.integer "post_id"
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
