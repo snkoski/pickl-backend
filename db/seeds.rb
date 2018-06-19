2d7c8281966d674f51e09591d24e82b6ef65a608
snkoski <shawnnkoski@gmail.com>
2018-06-19 19:21:15 -0400

    seeds?

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Post.destroy_all
+
+
+# User.create(name: "Abdoul", email: "fake@notarealemail.com")
+# User.create(name: "Shawn", email: "notrealemail@fake.com")
+#
+# Team.create(name: "Rockies", city: "Colorado", abbreviation: "COL")
+# Team.create(name: "Mets", city: "New York", abbreviation: "NYM")
+#
+# Post.create(home_team_id: 3, away_team_id: 4, game_time: "2017-06-15 11:25:30", home_likes: 0, away_likes: 0, post_likes: 0)
+# Post.create(home_team_id: 3, away_team_id: 1, game_time: "2017-07-01 12:25:30", home_likes: 0, away_likes: 0, post_likes: 0)
+# Post.create(home_team_id: 1, away_team_id: 4, game_time: "2017-08-20 01:25:30", home_likes: 0, away_likes: 0, post_likes: 0)
+# Post.create(home_team_id: 2, away_team_id: 4, game_time: "2017-06-18 09:25:30", home_likes: 0, away_likes: 0, post_likes: 0)
+
+# Likes.create(team_id: 2)
+# Like.create(team_id: 3)
+# Like.create(team_id: 3)
+# Like.create(team_id: 3)
+# Like.create(team_id: 3)
+# Like.create(team_id: 3)
+# Like.create(team_id: 4)
+# Like.create(team_id: 4)
+# Like.create(team_id: 4)
+# Like.create(team_id: 4)
+#
+# Comment.create(user_id: 2, post_id: 3, content: "Great game")
+# Comment.create(user_id: 3, post_id: 4, content: "Go local sports team")
+# Comment.create(user_id: 3, post_id: 5, content: "Oh, drats")
+# Comment.create(user_id: 2, post_id: 6, content: "Sick Yeah!")
+# Comment.create(user_id: 1, post_id: 4, content: "WOO HOO")
