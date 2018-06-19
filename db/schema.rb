58d1fc219cc8ef2e83b97d3940957c4031112a60
snkoski <shawnnkoski@gmail.com>
2018-06-19 16:18:02 -0400

    user model and tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_201449) do
+ActiveRecord::Schema.define(version: 2018_06_19_201648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2018_06_19_201449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "total_likes", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
