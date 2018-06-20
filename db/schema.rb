a633360ea8be329c2dd31b70f8465a3e76909575
snkoski <shawnnkoski@gmail.com>
2018-06-20 11:08:07 -0400

    add date and time columns to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_143535) do
+ActiveRecord::Schema.define(version: 2018_06_20_150709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_06_20_143535) do
     t.integer "post_likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
+    t.time "time"
   end
 
   create_table "teams", force: :cascade do |t|
