91fe0af0e6d11765b9e63deb17fd48e06d46d9c0
snkoski <shawnnkoski@gmail.com>
2018-06-21 11:46:22 -0400

    finished team abbreviations

---
+++ db/game_seeds_test.rb
@@ -0,0 +1,111 @@
+[
+{"id":"38171","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"7:05PM",
+
+"awayTeam":{"ID":"116","City":"Cleveland","Name":"Indians","Abbreviation":"CLE"},
+
+"homeTeam":{"ID":"111","City":"Baltimore","Name":"Orioles","Abbreviation":"BAL"},
+
+"location":"Oriole Park at Camden Yards"},
+Post.create(home_team_id: 111, away_team_id: 116, date:"2017-06-19", time: "7:05PM0", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+{"id":"39589","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"7:10PM",
+
+"awayTeam":{"ID":"126","City":"Washington","Name":"Nationals","Abbreviation":"WAS"},
+
+"homeTeam":{"ID":"128","City":"Miami","Name":"Marlins","Abbreviation":"MIA"},
+
+"location":"Marlins Park"},
+Post.create(home_team_id: 128, away_team_id: 126, date:"2017-06-19", time: "7:10PM0", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+{"id":"38606","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"7:10PM",
+
+"awayTeam":{"ID":"135","City":"Cincinnati","Name":"Reds","Abbreviation":"CIN"},
+
+"homeTeam":{"ID":"115","City":"Tampa Bay","Name":"Rays","Abbreviation":"TB"},
+
+"location":"Tropicana Field"},
+Post.create(home_team_id: 115, away_team_id: 135, date:"2017-06-19", time: "7:10PM0", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+{"id":"38017","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"7:35PM",
+
+"awayTeam":{"ID":"136","City":"San Francisco","Name":"Giants","Abbreviation":"SF"},
+
+"homeTeam":{"ID":"130","City":"Atlanta","Name":"Braves","Abbreviation":"ATL"},
+
+"location":"SunTrust Park"},
+Post.create(home_team_id: 130, away_team_id: 136, date:"2017-06-19", time: "7:35PM0", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+{"id":"39685","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"7:40PM",
+
+"awayTeam":{"ID":"132","City":"Pittsburgh","Name":"Pirates","Abbreviation":"PIT"},
+
+"homeTeam":{"ID":"134","City":"Milwaukee","Name":"Brewers","Abbreviation":"MIL"},
+
+"location":"Miller Park"},
+Post.create(home_team_id: 134, away_team_id: 132, date:"2017-06-19", time: "7:40PM0", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+{"id":"40204","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"8:05PM",
+
+"awayTeam":{"ID":"112","City":"Toronto","Name":"Blue Jays","Abbreviation":"TOR"},
+
+"homeTeam":{"ID":"121","City":"Texas","Name":"Rangers","Abbreviation":"TEX"},
+
+"location":"Globe Life Park in Arlington"},
+Post.create(home_team_id: 121, away_team_id: 112, date:"2017-06-19", time: "8:05PM0", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+{"id":"38472","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"8:05PM",
+
+"awayTeam":{"ID":"139","City":"San Diego","Name":"Padres","Abbreviation":"SD"},
+
+"homeTeam":{"ID":"131","City":"Chicago","Name":"Cubs","Abbreviation":"CHC"},
+
+"location":"Wrigley Field"},
+Post.create(home_team_id: 131, away_team_id: 139, date:"2017-06-19", time: "8:05PM0", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+{"id":"38321","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"8:15PM",
+
+"awayTeam":{"ID":"113","City":"Boston","Name":"Red Sox","Abbreviation":"BOS"},
+
+"homeTeam":{"ID":"118","City":"Kansas City","Name":"Royals","Abbreviation":"KC"},
+
+"location":"Kauffman Stadium"},
+Post.create(home_team_id: 118, away_team_id: 113, date:"2017-06-19", time: "8:15PM0", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+{"id":"39207","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"10:05PM",
+
+"awayTeam":{"ID":"122","City":"Houston","Name":"Astros","Abbreviation":"HOU"},
+
+"homeTeam":{"ID":"125","City":"Oakland","Name":"Athletics","Abbreviation":"OAK"},
+
+"location":"Oakland-Alameda County Coliseum"},
+Post.create(home_team_id: 125, away_team_id: 122, date:"2017-06-19", time: "10:05PM", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+
+{"id":"39498","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"10:10PM",
+
+"awayTeam":{"ID":"127","City":"New York","Name":"Mets","Abbreviation":"NYM"},
+
+"homeTeam":{"ID":"137","City":"Los Angeles","Name":"Dodgers","Abbreviation":"LAD"},
+
+"location":"Dodger Stadium"},
+Post.create(home_team_id: 137, away_team_id: 127, date:"2017-06-19", time: "10:10PM", home_likes: 0, away_likes: 0, post_likes: 0)
+
+
+
+{"id":"39088","scheduleStatus":"Normal","originalDate":null,"originalTime":null,"delayedOrPostponedReason":null,"date":"2017-06-19","time":"10:10PM",
+
+"awayTeam":{"ID":"117","City":"Detroit","Name":"Tigers","Abbreviation":"DET"},
+
+"homeTeam":{"ID":"123","City":"Seattle","Name":"Mariners","Abbreviation":"SEA"},
+
+"location":"Safeco Field"}]
+Post.create(home_team_id: 123, away_team_id: 117, date:"2017-06-19", time: "10:10PM", home_likes: 0, away_likes: 0, post_likes: 0)
